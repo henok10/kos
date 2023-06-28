83ebaafb05504d7fc9b1539398fc66a24c3ab3c9
awansk <sallekarurungd18h@student.unhas.ac.id>
2023-06-28 13:23:56 +0800

    bsdbsdbs

--- my_kos/settings.py
+++ my_kos/settings.py
@@ -152,7 +152,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build/static')
 ]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'frontend/build/static')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
