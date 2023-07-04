7cf78d7d2b162f3bc631e45f9d69ec009f1f4fe6
awansk <sallekarurungd18h@student.unhas.ac.id>
2023-07-04 17:23:48 +0800

    sfskjqfne32812

--- my_kos/settings.py
+++ my_kos/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-040u)!+$1^zlyr*o15hee==v%aq(l#(kx=#b^805tmkakth^#r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
