7ead7f0068ffa3449978546b1a50579c6f3b8351
awansk <sallekarurungd18h@student.unhas.ac.id>
2023-07-23 13:55:35 +0800

    'commit23ds2'

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -157,7 +157,7 @@ class SendPasswordResetEmailSerializer(serializers.Serializer):
             user = User.objects.get(email=email)
             uid = urlsafe_base64_encode(force_bytes(user.id))
             token = PasswordResetTokenGenerator().make_token(user)
-            link = f'http://localhost:3000/api/user/reset/{uid}/{token}'
+            link = f'https://mykos2.onrender.com/api/user/reset/{uid}/{token}'
             body = f'Click the following link to reset your password: {link}'
             recipient_email = user.email
 
