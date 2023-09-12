ba9fdf030e7bc5734bdc860192550c950b83efef
awansk <sallekarurungd18h@student.unhas.ac.id>
2023-09-12 14:39:01 +0800

    'cp13'

--- my_kos/settings.py
+++ my_kos/settings.py
@@ -183,7 +183,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'diawan.sallekarurung@gmail.com'
-EMAIL_HOST_PASSWORD = 'yqsubrmzaqucvgjd'
+EMAIL_HOST_PASSWORD = 'fdqklfjgsutbrwzg'
 EMAIL_USE_TLS = True
 
 GRAPH_MODELS = {
