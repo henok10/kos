101d6746a08988f4693511464dcdeb323067b86a
awansk <sallekarurungd18h@student.unhas.ac.id>
2023-10-06 13:02:26 +0800

    cp31

--- frontend/src/components/Topbar.jsx
+++ frontend/src/components/Topbar.jsx
@@ -2,13 +2,9 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { logout } from "../actions/auth";
-import {
-  Button,
-  Typography,
-  AppBar,
-  Toolbar,
-  Box,
-} from "@mui/material";
+import { Button, Typography, AppBar, Toolbar, Box } from "@mui/material";
+import mikos from "../data/mikos.png";
+import mikos1 from "../data/mikos.png";
 
 function Topbar() {
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
@@ -35,23 +31,36 @@ function Topbar() {
   );
 
   return (
-    <AppBar position="static" color="transparent" elevation={0}>
-      <Toolbar style={{ display: "flex", justifyContent: "space-between" }}>
+    <AppBar position="static" color="transparent" elevation={4}>
+      <Toolbar
+        style={{ display: "flex", left: 0, justifyContent: "space-between" }}
+      >
         <Box
           style={{
             display: "flex",
             justifyContent: "space-between",
-            width: "85%",
+            width: "100%",
+            padding: "5px"
           }}
         >
-          <Button color="inherit" onClick={() => navigate("/")}>
-            <Typography variant="h5">Kos Saya</Typography>
-          </Button>
+          <div
+            style={{
+              display: "flex",
+              position: "absolute",
+              width: "10rem",
+              left: 0,
+              top: 0,
+              alignItems: "center",
+            }}
+            color="inherit"
+            onClick={() => navigate("/")}
+          >
+            <img style={{ width: "100%"}} src={mikos1} alt="" />
+          </div>
           {/* <Button color="inherit" onClick={() => navigate("/syarat_ketentuan")}>
             Syarat dan Ketentuan
           </Button> */}
         </Box>
-        <Box style={{ backgroundColor: "red" }} />
         {isAuthenticated ? authLinks : publicLinks}
         {/* </Box> */}
       </Toolbar>
