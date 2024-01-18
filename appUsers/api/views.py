3a55760c4c2eef96732e47bbe6c224fe74017904
awansk <sallekarurungd18h@student.unhas.ac.id>
2024-01-19 01:59:11 +0800

    cp4

--- appUsers/api/views.py
+++ appUsers/api/views.py
@@ -60,6 +60,7 @@ class OwnerSignupView(generics.GenericAPIView):
             "user": UserSerializer(user, context=self.get_serializer_context()).data,
             "token": token,
             'user_id': user_id,
+            'username': username,
             'access_token':  access_token,
             'refresh_token': refresh_token,
             "message": "account created successfully"
